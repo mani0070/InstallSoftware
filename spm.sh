74aa8962923a4370f80d2c03ff4bba9898c27272
Manimaran Chandrasekaran <mani@dinventive.com>
2017-01-07 07:57:47 +0000

    Update spm.sh

--- spm.sh
+++ spm.sh
@@ -3,3 +3,10 @@ apt-get -y update
 
 # install mono
 apt-get -y install mono-complete 
+
+deb http://pub-repo.sematext.com/ubuntu sematext main
+
+wget -O - https://pub-repo.sematext.com/ubuntu/sematext.gpg.key | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install spm-client
