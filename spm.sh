a43ff74cb72efd71c951333623ba0bc743cfc50c
Manimaran Chandrasekaran <mani@dinventive.com>
2017-01-09 06:37:24 +0000

    Update spm.sh

--- spm.sh
+++ spm.sh
@@ -4,7 +4,7 @@ serverip=$1
 add-apt-repository "deb http://pub-repo.sematext.com/debian sematext main"
 wget -O- https://pub-repo.sematext.com/ubuntu/sematext.gpg.key | apt-key add -
 
-echo '${serverip} ${serverurl}' > /etc/hosts
+echo "${serverip} ${serverurl}" > /etc/hosts
 apt-get update
 # install mono
 apt-get install mono-complete -y 
