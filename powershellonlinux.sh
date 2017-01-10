35d9351beb84e9b911c4f2caf71dbd69d169ba48
Manimaran Chandrasekaran <mani@dinventive.com>
2017-01-10 07:06:28 +0000

    Update powershellonlinux.sh

--- powershellonlinux.sh
+++ powershellonlinux.sh
@@ -1,4 +1,5 @@
 sudo apt-get install libcurl3 -y
+sudo apt-get install libunwind8 libicu55 -y
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.14/powershell_6.0.0-alpha.14-1ubuntu1.16.04.1_amd64.deb
 sudo dpkg -i powershell_6.0.0-alpha.14-1ubuntu1.16.04.1_amd64.deb
 sudo apt-get install -f -y
