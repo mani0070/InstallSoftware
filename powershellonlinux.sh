5f028343edc39549a35addb682fb89f6dae308c8
Manimaran Chandrasekaran <mani@dinventive.com>
2017-01-10 07:05:30 +0000

    Create powershellonlinux.sh

---
+++ powershellonlinux.sh
@@ -0,0 +1,4 @@
+sudo apt-get install libcurl3 -y
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.14/powershell_6.0.0-alpha.14-1ubuntu1.16.04.1_amd64.deb
+sudo dpkg -i powershell_6.0.0-alpha.14-1ubuntu1.16.04.1_amd64.deb
+sudo apt-get install -f -y
